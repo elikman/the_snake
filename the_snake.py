d9347edf6fac9d5dba57bcfcc0594c997c9ac4e9
elikman <46077043+elikman@users.noreply.github.com>
2024-02-17 13:38:18 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -163,20 +163,20 @@ def main():
             snake.length += 1
             
         def handle_keys(self):
-        """Метод обработки нажатий клавиш."""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                raise SystemExit
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and self.direction != DOWN:
-                    self.next_direction = UP
-                elif event.key == pygame.K_DOWN and self.direction != UP:
-                    self.next_direction = DOWN
-                elif event.key == pygame.K_LEFT and self.direction != RIGHT:
-                    self.next_direction = LEFT
-                elif event.key == pygame.K_RIGHT and self.direction != LEFT:
-                    self.next_direction = RIGHT
+            """Метод обработки нажатий клавиш."""
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    raise SystemExit
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP and self.direction != DOWN:
+                        self.next_direction = UP
+                    elif event.key == pygame.K_DOWN and self.direction != UP:
+                        self.next_direction = DOWN
+                    elif event.key == pygame.K_LEFT and self.direction != RIGHT:
+                        self.next_direction = LEFT
+                    elif event.key == pygame.K_RIGHT and self.direction != LEFT:
+                        self.next_direction = RIGHT
         
         # Отрисовка на экране
         screen.fill(BOARD_BACKGROUND_COLOR)
